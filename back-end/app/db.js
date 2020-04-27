7cffb8c017ec5531831158e6b5c7d674b88937c7
Alexis Okamura <alexiskokamura@gmail.com>
2020-04-26 19:48:57 -0700

    setup db

---
+++ back-end/app/db.js
@@ -0,0 +1,24 @@
+const { MongoClient } = require('mongodb');
+const config = require('./config/config.json');
+const env = process.env.NODE_ENV || 'development';
+
+const URI = `mongodb+srv://${config[env].username}:${config[env].password}@accountable-ei8ox.mongodb.net/test?retryWrites=true&w=majority`;
+
+const initialize = (dbName, dbCollectionName, successCB, failureCB) => {
+  MongoClient.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true }, (error, db) => {
+    if (error) {
+      console.log(`[MongoDB connection] ERROR: ${error}`);
+      failureCB(error);
+    } else {
+      const database = db.db(dbName);
+      const collection = database.collection(dbCollectionName);
+      console.log('[MongoDB connection] SUCCESS');
+
+      successCB(collection);
+    }
+  });
+};
+
+module.exports = {
+  initialize
+};
--- back-end/app/server.js
+++ back-end/app/server.js
@@ -1,9 +1,24 @@
 const bodyParser = require('body-parser');
+const config = require('./config/config.json');
+const env = process.env.NODE_ENV || 'development';
 const express = require('express');
 
-let app = express();
+const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(3000, () => {});
+const db = require('./db');
+const dbName = 'accountable-data';
+const dbCollectionName = 'users';
+
+app.listen(config[env].port, async () => {
+    console.log(`SERVER: Listening on ${config[env].port}`);
+
+    db.initialize(dbName, dbCollectionName, (collection) => {
+        collection.find().toArray((error, result) => {
+            if (error) throw error;
+            console.log('[MongoDB]', result);
+        });
+    }, (error) => { throw error });
+});
