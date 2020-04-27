1758a92ff7cb845a78918c1b372f8a8999c8590e
Alexis Okamura <alexiskokamura@gmail.com>
2020-04-26 20:25:30 -0700

    create users model

---
+++ back-end/app/models/users.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+module.exports = mongoose.model('users', 
+  new Schema({
+    first_name: {
+      type: String,
+      required: true
+    },
+    last_name: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    created_at: {
+      type: Date,
+      default: Date.now
+    }
+  }));
