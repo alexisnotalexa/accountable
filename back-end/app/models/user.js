257d79168d65f1e00e46330e131aec462740260a
Alexis Okamura <alexiskokamura@gmail.com>
2020-05-02 00:25:27 -0700

    Add error messages and validator for required fields

--- back-end/app/models/user.js
+++ back-end/app/models/user.js
@@ -1,26 +1,30 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-// TODO: Add custom validators and messages for required fields.
-
 module.exports = mongoose.model('User', 
   new Schema({
     firstName: {
       type: String,
-      required: true
+      required: [true, 'First name field is required.']
     },
     lastName: {
       type: String,
-      required: true
+      required: [true, 'Last name field is required.']
     },
     email: {
       type: String,
-      required: true,
+      required: [true, 'A valid email address is required.'],
       unique: true
     },
     password: {
       type: String,
-      required: true
+      required: [true, 'A valid password is required.'],
+      validate: {
+        validator: (password) => {
+          return password.length >= 6;
+        },
+        message: 'Your password must be at least 6 characters.'
+      }
     }
   }, { timestamps: true })
 );
