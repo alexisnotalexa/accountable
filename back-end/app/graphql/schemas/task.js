bd0b923f99c7c3f1a618f8f906fdf8c38bd3b759
Alexis Okamura <alexiskokamura@gmail.com>
2020-05-10 19:08:26 -0700

    start writing task schema

--- back-end/app/graphql/schemas/task.js
+++ back-end/app/graphql/schemas/task.js
@@ -8,4 +8,25 @@ module.exports = gql`
     createdAt: String!
     updatedAt: String!
   }
+
+  input NewTaskInput {
+    description: String!
+    groupId: ID!
+  }
+
+  input UpdateTaskInput {
+    description: String!
+    groupId: ID!
+  }
+
+  extend type Query {
+    getAllTasksForGroup(groupId: ID!): [Task!]
+    getAllTasksForUser(userId: ID!): [Task!]
+  }
+
+  extend type Mutation {
+    createTask(task: NewTaskInput): Task!
+    updateTask(task: UpdateTaskInput): Task!
+    deleteTask(id: ID!): Boolean!
+  }
 `;
