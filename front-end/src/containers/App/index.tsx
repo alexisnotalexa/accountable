094ef870043e0ebd67163cdae70af4616d31a5e3
Alexis Okamura <alexiskokamura@gmail.com>
2020-05-25 08:07:56 -0700

    add basic structure and styling for home page

--- front-end/src/containers/App/index.tsx
+++ front-end/src/containers/App/index.tsx
@@ -1,19 +1,41 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-import Logo from '../../components/Logo';
+import Logo from "../../components/Logo";
 
 function App() {
   return (
     <div className="App">
-      <Logo />
-      <Logo small />
-      <p>I'm baby edison bulb PBR&B hammock knausgaard jean shorts humblebrag bicycle rights. Meggings jean shorts tattooed, four dollar toast banh mi shoreditch tilde locavore intelligentsia kitsch. Kitsch tousled jean shorts, artisan marfa celiac biodiesel street art. Ennui pitchfork 90's chicharrones. Paleo hoodie actually hot chicken.
-
-Craft beer chambray selvage chillwave cloud bread. Aesthetic gluten-free tumblr paleo meh cloud bread. Plaid organic actually, cloud bread mumblecore keffiyeh roof party helvetica 8-bit heirloom shoreditch. Etsy lyft migas, vinyl wolf copper mug hell of.
-
-Offal stumptown poke, put a bird on it succulents twee tbh small batch fashion axe blog skateboard pork belly. Small batch prism keytar, lyft plaid blue bottle pug mumblecore put a bird on it helvetica. Pabst tbh tilde XOXO put a bird on it. Fingerstache kale chips PBR&B, chambray pitchfork irony authentic scenester gochujang hexagon hoodie taxidermy.</p>
-      <code>Blah blah test</code>
+      <main>
+        <aside>
+          <Logo />
+          <nav>
+            <span>Home</span>
+          </nav>
+        </aside>
+        <div className="content">
+          <p>
+            I'm baby edison bulb PBR&B hammock knausgaard jean shorts humblebrag
+            bicycle rights. Meggings jean shorts tattooed, four dollar toast
+            banh mi shoreditch tilde locavore intelligentsia kitsch. Kitsch
+            tousled jean shorts, artisan marfa celiac biodiesel street art.
+            Ennui pitchfork 90's chicharrones. Paleo hoodie actually hot
+            chicken. Craft beer chambray selvage chillwave cloud bread.
+            Aesthetic gluten-free tumblr paleo meh cloud bread. Plaid organic
+            actually, cloud bread mumblecore keffiyeh roof party helvetica 8-bit
+            heirloom shoreditch. Etsy lyft migas, vinyl wolf copper mug hell of.
+            Offal stumptown poke, put a bird on it succulents twee tbh small
+            batch fashion axe blog skateboard pork belly. Small batch prism
+            keytar, lyft plaid blue bottle pug mumblecore put a bird on it
+            helvetica. Pabst tbh tilde XOXO put a bird on it. Fingerstache kale
+            chips PBR&B, chambray pitchfork irony authentic scenester gochujang
+            hexagon hoodie taxidermy.
+          </p>
+        </div>
+      </main>
+      <footer>
+        <span>blah blah footer</span>
+      </footer>
     </div>
   );
 }
