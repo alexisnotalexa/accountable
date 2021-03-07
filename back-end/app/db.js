a5f8f651ccf3cb43df615ce685ca8be4d533edaa
Alexis Okamura <alexiskokamura@gmail.com>
2021-03-07 11:18:31 -0800

    update db connection url

--- back-end/app/db.js
+++ back-end/app/db.js
@@ -2,7 +2,7 @@ const { MongoClient } = require('mongodb');
 const config = require('./config/config.json');
 const env = process.env.NODE_ENV || 'development';
 
-const URI = `mongodb+srv://${config[env].username}:${config[env].password}@accountable-ei8ox.mongodb.net/test?retryWrites=true&w=majority`;
+const URI = `mongodb+srv://${config[env].username}:${config[env].password}@accountable.ei8ox.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const initialize = (dbName, dbCollectionName, successCB, failureCB) => {
   MongoClient.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true }, (error, db) => {
