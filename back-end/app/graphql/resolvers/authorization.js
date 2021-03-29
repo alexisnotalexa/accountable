1ec77a20c7e06115d25b198aa3d5f90a50ba6d41
Alexis Okamura <alexiskokamura@gmail.com>
2021-03-28 21:34:18 -0700

    update verbiage for isAuthenticated

--- back-end/app/graphql/resolvers/authorization.js
+++ back-end/app/graphql/resolvers/authorization.js
@@ -2,7 +2,7 @@ const { ForbiddenError } = require('apollo-server-express');
 const { combineResolvers, skip } = require('graphql-resolvers');
 
 const isAuthenticated = (_, args, { authUser }) =>
-  authUser ? skip : new ForbiddenError('Please login to have access.');
+  authUser ? skip : new ForbiddenError('You must provide an authorization token.');
 
 const isAdmin = combineResolvers(
   isAuthenticated,
